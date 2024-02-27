bdfe1148f2952939749ce42d721621cf05a43076
Ahmet Ağgöz <79806216+ahmetaggoz@users.noreply.github.com>
2024-02-27 20:30:10 +0300

    api ucreti odenemedigi icin proje baska bir zaman tamamlanacaktir

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,15 +1,64 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductService } from '../../../services/product/product.service';
+import * as sql from 'mssql';
+
+
+const config: sql.config = {
+  server: 'your_server_name',
+  database: 'your_database_name',
+  user: 'your_username',
+  password: 'your_password',
+  options: {
+    encrypt: true, // Enable encryption if needed
+  },
+};
+
+async function connectToDatabase() {
+  try {
+    await sql.connect(config);
+    console.log('Connected to the database successfully!');
+    // Perform database operations here
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+}
+
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
   isSidePanelVisible: boolean = false;
+
+  productObj: any = {
+    "productId": 0,
+    "productSku": "",
+    "productName": "",
+    "productPrice": 0,
+    "productShortName": "",
+    "productDescription": "",
+    "createdDate": new Date(),
+    "deliveryTimeSpan": "",
+    "categoryId":0,
+    "productImageUrl": "",
+  };
+  categoryList: any [] = [];
+  constructor(private productSrv: ProductService){}
+  ngOnInit(): void {
+    this.getAllCategory();
+    connectToDatabase();
+  }
+  getAllCategory(){
+    this.productSrv.getCategory().subscribe((res:any) =>{
+      this.categoryList = res.data;
+    })
+  }
 
   openSidePanel(){
     this.isSidePanelVisible = true;
---
+++ src/app/services/constant/constant.ts
@@ -0,0 +1,7 @@
+export const Constant = {
+    API_END_POINT: 'https://freeapi.miniprojectideas.com/api/BigBasket/',
+    METHODS: {
+        GET_ALL_PRODUCT: '',
+        GET_ALL_CATEGORY: 'GetAllCategory',
+    }
+}
